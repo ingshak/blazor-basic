1e363e02e42dcf1be8e0accdd6cb23ba43740df8
Omar Hamden <ohamden@smg.com>
2022-06-27 18:32:59 -0500

    api

--- blazor-basic/Client/Program.cs
+++ blazor-basic/Client/Program.cs
@@ -1,4 +1,4 @@
-
+ feed
 using BlazorBasic.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -8,6 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:32752/api/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://blazorpocapi.azurewebsites.net/api/") });
 
 await builder.Build().RunAsync();
