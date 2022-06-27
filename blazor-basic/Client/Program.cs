76b3b3ba64bfe4126627d4a99bb7f2a08672cb7f
Omar Hamden <ohamden@smg.com>
2022-06-27 14:20:33 -0500

    initial

--- blazor-basic/Client/Program.cs
+++ blazor-basic/Client/Program.cs
@@ -1,4 +1,5 @@
-using BlazorBasic;
+
+using BlazorBasic.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:32752/api/") });
 
 await builder.Build().RunAsync();
